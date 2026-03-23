539c026312996bfe8e2907856b025db5623b7180
Fx540 <makra.adi@gmail.com>
2026-03-23 13:05:44 +0100

    Mappa struktura
    #2

--- src/utils/constants.js
+++
@@ -1,5 +0,0 @@
-/**
- * Path to the default car image when none is provided or when image fails to load.
- * File lives in public/images/ so it is served at /images/car-placeholder.svg
- */
-export const CAR_PLACEHOLDER_IMAGE = '/images/car-placeholder.svg'
