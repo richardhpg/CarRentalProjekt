de6003599387eaff70feebd1e6e27c6dc7695077
P01ntlss <trabachrichard@gmail.com>
2026-03-30 13:14:47 +0200

    auth pass check done

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
 import jwt from "json-web-token"
 import { prisma } from "../lib/prisma.js"
-import { Request, response, Response } from "express"
+import { Request, Response } from "express"
 
 const SALT = 10
 
@@ -10,6 +10,7 @@ export const register = async (req: Request, res: Response) => {
         const { name, age, contact_email, contact_phoneNumber, password } = req.body;
 
         const passwordHash = await bcrypt.hash(password, SALT)
+        console.log(passwordHash)
 
         const newUser = await prisma.users.create({
             data: {
@@ -29,29 +30,29 @@ export const register = async (req: Request, res: Response) => {
 
 export const login = async (req: Request, res: Response) => {
     try {
-        const { email, password } = req.body
+        const { contact_email, password } = req.body
 
-        const hashedPassword = await bcrypt.hash(password, SALT)
+        console.log(password)
 
         const user = await prisma.users.findUnique({
             where: {
-                email: email
+                contact_email: contact_email
             }
         })
 
-        if (user == null || user == "" || user == 0) {
-            response.status(400).json({ message: "Nem megfelelő e-mail vagy jelszó" })
+        if (!user) {
+           return res.status(400).json({ message: "Nem megfelelő e-mail" })
         }
-        else {  
-            if (user.password == hashedPassword) {
-                res.status(200).json({message: "Sikeres bejelentkezés"})
-            }
-            else
-            {
-                response.status(400).json({ message: "Nem megfelelő e-mail vagy jelszó" })
-            }
+        let match = await bcrypt.compare(password, user.password);
+        console.log(match)  
+        if (!match) {
+            return res.status(400).json({ message: "Nem megfelelő jelszó" })
+           
         }
-    } catch (err: any) {
+
+         return res.status(200).json({message: "Sikeres bejelentkezés"})
+        }
+    catch (err: any) {
         res.status(500).json(err.message)
     }
 }
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,12 @@
 import {
-    register
+    register,
+    login
 } from "../controllers/authController.js"
 import { Router } from "express"
 
 const router = Router();
 
 router.post("/register", register)
+router.post("/login", login)
 
 export default router;
