f71d1ccda1ed4ee2a543e7b90019a45d5949e6cc
P01ntlss <trabachrichard@gmail.com>
2026-03-31 11:09:09 +0200

    auth Tokens

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import jwt from "json-web-token"
+import jwt from "jsonwebtoken"
 import { prisma } from "../lib/prisma.js"
 import { Request, Response } from "express"
 
@@ -32,8 +32,6 @@ export const login = async (req: Request, res: Response) => {
     try {
         const { contact_email, password } = req.body
 
-        console.log(password)
-
         const user = await prisma.users.findUnique({
             where: {
                 contact_email: contact_email
@@ -41,17 +39,36 @@ export const login = async (req: Request, res: Response) => {
         })
 
         if (!user) {
-           return res.status(400).json({ message: "Nem megfelelő e-mail" })
+            return res.status(400).json({ message: "Nem megfelelő e-mail vagy jelszo" })
         }
         let match = await bcrypt.compare(password, user.password);
-        console.log(match)  
         if (!match) {
-            return res.status(400).json({ message: "Nem megfelelő jelszó" })
-           
-        }
+            return res.status(400).json({ message: "Nem megfelelő email vagy jelszó" })
 
-         return res.status(200).json({message: "Sikeres bejelentkezés"})
+
         }
+        const accessToken = jwt.sign({ id: user.id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" })
+        const refreshToken = jwt.sign({ id: user.id }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "5d" })
+        const test = new Date(new Date().setDate(new Date().getDate() + 7))
+        console.log(test)
+
+        await prisma.refresh_token.create({
+            data: {
+                token: refreshToken,
+                user_id: user.id,
+                expiresAt: test
+            }
+        })
+
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            secure: false,
+            path: '/refresh', 
+            maxAge: 7 * 24 * 60 * 60 * 1000 
+        });
+
+        return res.status(200).json(accessToken)
+    }
     catch (err: any) {
         res.status(500).json(err.message)
     }
