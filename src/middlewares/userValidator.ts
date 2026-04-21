286376f09bd8f1962d0d09cf7fede6b55139e8ba
P01ntlss <108630516+richardhpg@users.noreply.github.com>
2026-04-21 14:20:19 +0200

    test

--- src/middlewares/userValidator.ts
+++ src/middlewares/userValidator.ts
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 
 export const refresh( async(req:Request,res:Response) => {
     try {
-        const accessToken = 
+        const accessToken = "asd"
     } catch (err:any) {
         response.status(500).json({message: err.message})
     }
